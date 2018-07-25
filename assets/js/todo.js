73a5c717397bca3eedd28b0b9f718788cfdf339b
Chad Chaney <cchane4@gmail.com>
2018-07-25 09:39:03 -0500

    adding fadeout and delete functionality

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -5,9 +5,14 @@ $("li").click(function(){
 }); 
 
 $('span').click(function(event){ 
-    alert("clicked on a span!")
+    // here this refers to the span element
+    $(this).parent().fadeOut(500,function(){
+        // here this refers to the li element  
+        $(this).remove(); 
+    }) 
     event.stopPropagation(); 
     // stopPropagation and passing in event as a parameter of the function
     // stops the event from "bubbling up" to the other parent elements of the 
     // span tag (ex:ul, li, body etc.) which is called event leveling
 }); 
+
