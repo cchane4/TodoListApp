2f3e3cb25350cd003d0db37919e06743e795a0c3
Chad Chaney <cchane4@gmail.com>
2018-07-25 09:28:19 -0500

    adding event listening to span tag

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -3,3 +3,11 @@ $("li").click(function(){
     // if li is grey turn it black else turn it gray
     $(this).toggleClass("completed"); 
 }); 
+
+$('span').click(function(event){ 
+    alert("clicked on a span!")
+    event.stopPropagation(); 
+    // stopPropagation and passing in event as a parameter of the function
+    // stops the event from "bubbling up" to the other parent elements of the 
+    // span tag (ex:ul, li, body etc.) which is called event leveling
+}); 
