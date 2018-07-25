b0445c5d038f14d7fcf8b4babce8b82f22a268ec
Chad Chaney <cchane4@gmail.com>
2018-07-25 09:14:28 -0500

    easy way to toggle class

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,16 +1,5 @@
 //Check off todos by clicking 
 $("li").click(function(){ 
     // if li is grey turn it black else turn it gray
-    if ($(this).css("color") === "rgb(128, 128, 128)"){ 
-       $(this).css({ 
-           color: "black", 
-           textDecoration: "none"
-         }); 
-     
-    } else { 
-    $(this).css({ 
-        color: "gray", 
-        textDecoration: "line-through"
-    }) 
-  } 
+    $(this).toggleClass("completed"); 
 }); 
