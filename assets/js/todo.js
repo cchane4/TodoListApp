968053167d4df858f4c338ab3f0aee992e3d9427
Chad Chaney <cchane4@gmail.com>
2018-07-25 10:53:23 -0500

    adding on click event indead of click

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,18 +1,26 @@
 //Check off todos by clicking 
-$("li").click(function(){ 
+$("ul").on("click", "li", function(){ 
     // if li is grey turn it black else turn it gray
     $(this).toggleClass("completed"); 
 }); 
 
-$('span').click(function(event){ 
+$("ul").on("click", "span", function(event){ 
     // here this refers to the span element
     $(this).parent().fadeOut(500,function(){
         // here this refers to the li element  
         $(this).remove(); 
-    }) 
+    });  
     event.stopPropagation(); 
-    // stopPropagation and passing in event as a parameter of the function
-    // stops the event from "bubbling up" to the other parent elements of the 
-    // span tag (ex:ul, li, body etc.) which is called event leveling
+    
 }); 
 
+$("input[type='text'").keypress(function(event){ 
+    if(event.which === 13){ 
+ var todoText = $(this).val();
+ $(this).val(""); 
+ $("ul").append("<li><span>X</span> "+ todoText +"</li>"); 
+    }
+})
+
+/* here the functionality changed from click to on click so that the functions can work on 
+newly created tasks in the code */
