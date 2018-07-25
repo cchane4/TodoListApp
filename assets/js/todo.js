c39de0bed30812cf6202a76c59c1f05ce2a33bb8
Chad Chaney <cchane4@gmail.com>
2018-07-25 09:11:11 -0500

    long way of adding toggle functionality - Not necessary

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,7 +1,16 @@
 //Check off todos by clicking 
 $("li").click(function(){ 
-    $(this).css("color","gray"); 
-    $(this).css("text-decoration","line-through"); 
-
-
+    // if li is grey turn it black else turn it gray
+    if ($(this).css("color") === "rgb(128, 128, 128)"){ 
+       $(this).css({ 
+           color: "black", 
+           textDecoration: "none"
+         }); 
+     
+    } else { 
+    $(this).css({ 
+        color: "gray", 
+        textDecoration: "line-through"
+    }) 
+  } 
 }); 
